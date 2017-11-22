92a1b916eea26f100ee94477a480895d7a9bfd81
Frédéric DROUET <fdrouet@exoplatform.com>
2017-11-22 23:54:40 +0100

    Add a way to specify the uid for jmeter user
    - create the user before starting jmeter
    - use gosu to switch from root to the specified user
    - ability to specify the user UID with LOCAL_USER_ID (default: 1000)
    - ability to specify the user username with LOCAL_USER_NAME (default: jmeter)
    - improve the doc
    - add a way to start bash instead of jmeter for image debuging purpose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,30 @@
 #!/bin/bash -eu
 
-# WARNING Not supporting spaces
-PROPERTIES=$(env | grep "^${JMETER_PROPERTY_PREFIX:-NOT_EXISTS}" || true)
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or fallback
+USER_ID=${LOCAL_USER_ID:-1000}
+USER_NAME=${LOCAL_USER_NAME:-jmeter}
+echo "Starting with UID=$USER_ID($USER_NAME)"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m $USER_NAME
 
-PROPERTIES_STRING=""
-for i in ${PROPERTIES}
-do
-    PROPERTIES_STRING="${PROPERTIES_STRING} -J$(echo ${i} | sed s/${JMETER_PROPERTY_PREFIX}//g)"
-done
+# allow starting the container with bash (for Docker image debuging purpose)
+if [[ ${@:-nothing} == bash* ]]; then
+    shift # we remove bash from the arguments
+    [ ! -z "$@" ] && echo "Starting bash but ignoring other parameters ($@)"
+    bash
+else
+    # WARNING Not supporting spaces
+    PROPERTIES=$(env | grep "^${JMETER_PROPERTY_PREFIX:-NOT_EXISTS}" || true)
 
-if [ ! -z "${PROPERTIES_STRING}" ]; then
-    echo Executing with additional properties : ${PROPERTIES_STRING}
+    PROPERTIES_STRING=""
+    for i in ${PROPERTIES}
+    do
+        PROPERTIES_STRING="${PROPERTIES_STRING} -J$(echo ${i} | sed s/${JMETER_PROPERTY_PREFIX}//g)"
+    done
+
+    if [ ! -z "${PROPERTIES_STRING}" ]; then
+        echo Executing with additional properties : ${PROPERTIES_STRING}
+    fi
+
+    exec /usr/local/bin/gosu $USER_NAME /usr/local/jmeter/bin/jmeter ${PROPERTIES_STRING} "$@"
 fi
-
-/usr/local/jmeter/bin/jmeter -n ${PROPERTIES_STRING} "$@"
